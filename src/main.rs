648ff3b33af7d70cb07200fafdfec02a1dac3f27
pott <pott@cluml.com>
2025-03-05 11:34:02 +0900

    SharedStates 커넥션에 성공한 모든 클라이언트가 HashMap에 값을 저장하고 공유할 수 있게 변경

--- src/main.rs
+++ src/main.rs
@@ -1,26 +1,37 @@
-use tokio::net::{TcpListener, TcpStream};
+use std::{
+    collections::HashMap,
+    sync::{Arc, Mutex},
+};
+
+use bytes::Bytes;
 use mini_redis::{Connection, Frame};
+use tokio::net::{TcpListener, TcpStream};
+
+type Db = Arc<Mutex<HashMap<String, Bytes>>>;
 
 #[tokio::main]
 async fn main() {
     // 소켓 통신을 바인딩
     let listener = TcpListener::bind("127.0.0.1:6379").await.unwrap();
+    println!("Listening");
+
+    let db = Arc::new(Mutex::new(HashMap::new()));
 
     loop {
         // 두번째 아이템이 IP 와 port 정보를 갖고 있다.
         let (socket, _) = listener.accept().await.unwrap();
+
+        let db = db.clone();
+
         // 연결될때 마다 새로운 비동기 작업이 spawn 되서 소유권을 넘겨받아 처리한다.
         tokio::spawn(async move {
-            process(socket).await;
+            process(socket, db).await;
         });
     }
 }
 
-async fn process(socket: TcpStream) {
+async fn process(socket: TcpStream, db: Db) {
     use mini_redis::Command::{self, Get, Set};
-    use std::collections::HashMap;
-
-    let mut db = HashMap::new();
 
     let mut connection = Connection::new(socket);
 
@@ -28,16 +39,14 @@ async fn process(socket: TcpStream) {
     while let Some(frame) = connection.read_frame().await.unwrap() {
         let response = match Command::from_frame(frame).unwrap() {
             Set(cmd) => {
-                // The value is stored as `Vec<u8>`
-                db.insert(cmd.key().to_string(), cmd.value().to_vec());
+                let mut db = db.lock().unwrap();
+                db.insert(cmd.key().to_string(), cmd.value().clone());
                 Frame::Simple("OK".to_string())
             }
             Get(cmd) => {
+                let db = db.lock().unwrap();
                 if let Some(value) = db.get(cmd.key()) {
-                    // `Frame::Bulk` expects data to be of type `Bytes`. This
-                    // type will be covered later in the tutorial. For now,
-                    // `&Vec<u8>` is converted to `Bytes` using `into()`.
-                    Frame::Bulk(value.clone().into())
+                    Frame::Bulk(value.clone())
                 } else {
                     Frame::Null
                 }
