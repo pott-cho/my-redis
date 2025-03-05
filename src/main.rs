a5e9175d2221b0eecfb3cd675430c586c7732fbc
pott <pott@cluml.com>
2025-03-05 11:00:41 +0900

    Store values - HashMap HashMap 을 이용해서 key-value 쌍을 인메모리에 저장할 수 있게 변경

--- src/main.rs
+++ src/main.rs
@@ -9,7 +9,7 @@ async fn main() {
     loop {
         // 두번째 아이템이 IP 와 port 정보를 갖고 있다.
         let (socket, _) = listener.accept().await.unwrap();
-        // 연결될때 마다 새로운 비동기 작업이 spawn 되서 소유권을 넘겨받아 처리한다. 
+        // 연결될때 마다 새로운 비동기 작업이 spawn 되서 소유권을 넘겨받아 처리한다.
         tokio::spawn(async move {
             process(socket).await;
         });
@@ -17,13 +17,35 @@ async fn main() {
 }
 
 async fn process(socket: TcpStream) {
+    use mini_redis::Command::{self, Get, Set};
+    use std::collections::HashMap;
+
+    let mut db = HashMap::new();
+
     let mut connection = Connection::new(socket);
 
-    if let Some(frame) = connection.read_frame().await.unwrap() {
-        println!("GOT: {:?}", frame);
+    // Use `read_frame` to receive a command from the connection.
+    while let Some(frame) = connection.read_frame().await.unwrap() {
+        let response = match Command::from_frame(frame).unwrap() {
+            Set(cmd) => {
+                // The value is stored as `Vec<u8>`
+                db.insert(cmd.key().to_string(), cmd.value().to_vec());
+                Frame::Simple("OK".to_string())
+            }
+            Get(cmd) => {
+                if let Some(value) = db.get(cmd.key()) {
+                    // `Frame::Bulk` expects data to be of type `Bytes`. This
+                    // type will be covered later in the tutorial. For now,
+                    // `&Vec<u8>` is converted to `Bytes` using `into()`.
+                    Frame::Bulk(value.clone().into())
+                } else {
+                    Frame::Null
+                }
+            }
+            cmd => panic!("unimplemented {:?}", cmd),
+        };
 
-        // Error 를 반환한다.
-        let response = Frame::Error("unimplemented".to_string());
+        // Write the response to the client
         connection.write_frame(&response).await.unwrap();
     }
 }
